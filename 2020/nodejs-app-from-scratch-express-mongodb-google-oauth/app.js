ebd593d22368e02c5e6960dac5b402d62c5c41e3
Novailoveyou <novailoveoux@gmail.com>
2021-03-04 21:07:03 +0300

    2020 nodejs-app-from-scratch: format date handlebar helper added

--- 2020/nodejs-app-from-scratch-express-mongodb-google-oauth/app.js
+++ 2020/nodejs-app-from-scratch-express-mongodb-google-oauth/app.js
@@ -28,8 +28,15 @@ if (process.env.NODE_ENV === 'development') {
   app.use(morgan('dev'))
 }
 
+// Handlebards Helpers
+const { formatDate } = require('./helpers/hbs')
+
 // Handlebars
-app.engine('.hbs', exphbs({ defaultLayout: 'main', extname: '.hbs' }))
+app.engine('.hbs', exphbs({
+  helpers: {
+    formatDate
+  }, defaultLayout: 'main', extname: '.hbs'
+}))
 app.set('view engine', '.hbs')
 
 // Sessions
